850a4450e34af1b17c1123dd4a626d61f777dc20
tomhoon <gnsdl9108@naver.com>
2025-11-08 08:16:51 +0900

    스플래시 첫페이지 완료

---
+++ src/app/splash/page.tsx
@@ -0,0 +1,19 @@
+import styles from '@/style/page/splash/Splash.module.scss'
+
+export default function () {
+  return (<div className={ styles.container }>
+    <ul className={ styles.landingWrapper }>
+      <li className={styles.logo}>
+        <div className={styles.logoWrapper}>
+          <img src="/images/LandingLogo.png" alt="" />
+        </div>
+      </li>
+      <li className={ styles.hotelName }>
+        <p>Grand Hotel</p>
+      </li>
+      <li className={ styles.hotelSub }>
+        <p>Find Your Perfect Stay, Anytime, Anywhere</p>
+      </li>
+    </ul>
+  </div>)
+}
